153b4a28ab69c3b4a3ae609abe3946c0bcf52876
Julieta2200 <julieta@megista.io>
2023-12-18 20:04:38 +0400

    player movement

--- player/player.gd
+++ player/player.gd
@@ -1,22 +1,25 @@
 extends CharacterBody2D
 
 
-var speed = 200
-var click_mous_pos = null
+var speed : int = 200
+var click_mouse_position : Vector2
 
+
+func _physics_process(_delta):
+	if click_mouse_position:
+		_movement()
+
+
+
+
+#player movement
+func _movement():
+		var direction = (click_mouse_position - global_position)
+		velocity = direction.normalized() * speed
+		move_and_slide()
+
+#left mouse click controls
 func  _input(event):
 	if event.is_action_pressed("left_click"):
-		click_mous_pos = get_viewport().get_mouse_position()
+		click_mouse_position = get_viewport().get_mouse_position()
 	
-	
-func _physics_process(_delta):
-	if click_mous_pos:
-		var direction_vector = (click_mous_pos - global_position)
-		
-		if direction_vector.length() < 3:
-			return
-			
-		velocity = direction_vector.normalized() * speed
-		move_and_slide()
-		
-		
