a99c186f648cd50a6d642748992b47e33591bad1
grishamegista <grisha@megista.io>
2023-12-21 21:44:09 +0400

    player movement

--- project/player/player.gd
+++ project/player/player.gd
@@ -1,2 +1,29 @@
 extends CharacterBody2D
 
+@export var speed: float
+var target_position: Vector2
+var position_delta: float
+
+func _ready():
+	target_position = position
+	position_delta = speed / 60 # game is working approximately in 60 fps
+
+func _physics_process(_delta):
+	move()
+
+func move() -> void:
+	var direction: Vector2 = (target_position - position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+	
+	if position.distance_to(target_position) < position_delta:
+		position = target_position #Stopped moving
+	
+	position = position.snapped(Vector2(1,1))
+	
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			target_position = get_global_mouse_position()
+
