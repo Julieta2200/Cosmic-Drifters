cce89964235560a8f87ed786c723b01ab6fb4f6b
Julieta2200 <julieta@megista.io>
2024-01-11 14:41:57 +0400

    tarsier click move

--- project/GM/game_manager.gd
+++ project/GM/game_manager.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var player: Player  = $"../player"
 @onready var tarsier: Tarsier = $"../tarsier"
 signal click_area_input_event(_viewport, event, _shape_idx)
-signal click_tarsier(_viewport, event, _shape_idx)
+signal click_tarsier_area(_viewport, event, _shape_idx)
 var on_area_tarsier: bool 
 
 func player_start_movement():
@@ -14,12 +14,10 @@ func tarsier_start_movement():
 
 func _on_walking_area_input_event(_viewport, event, _shape_idx):
 	if on_area_tarsier:
-		click_tarsier.emit(_viewport, event, _shape_idx)
+		click_tarsier_area.emit(_viewport, event, _shape_idx)
 		on_area_tarsier = false
 	else:
 		click_area_input_event.emit(_viewport, event, _shape_idx)
 
-
-func _on_tarsier_click_tarsier_input_event(_viewport, _event, _shape_idx):
+func _on_click_tarsier_input_event(_viewport, _event, _shape_idx):
 	on_area_tarsier = true
-	
--- project/GM/mouse_manager.gd
+++ project/GM/mouse_manager.gd
@@ -12,7 +12,7 @@ func _on_game_manager_click_area_input_event(_viewport, event, _shape_idx):
 				$"..".player_start_movement()
 
 
-func _on_game_manager_click_tarsier(_viewport, event, _shape_idx):
+func _on_game_manager_click_tarsier_area(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			on_click_tarsier = true
