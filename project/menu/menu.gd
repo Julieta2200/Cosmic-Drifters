8ad73f07dc117c7a510c4bfd88e74293bb7b9474
Julieta2200 <julieta@megista.io>
2024-04-14 18:01:01 +0400

    settings

--- project/menu/menu.gd
+++ project/menu/menu.gd
@@ -4,3 +4,17 @@ extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://project/level_1/level_1.tscn")
+
+
+
+
+func _on_back_button_pressed():
+	$Background.set_modulate("ffffff")
+	$Settings.visible = false
+	$VBoxContainer.visible = true
+
+
+func _on_settings_button_pressed():
+	$Background.set_modulate("ffffff5b")
+	$Settings.visible =  true
+	$VBoxContainer.visible = false
