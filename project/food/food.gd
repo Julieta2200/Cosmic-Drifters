95ddb156e99c8f426df404a6203ba2f21ee32661
Julieta2200 <julieta@megista.io>
2024-05-03 22:41:20 +0400

    food

--- project/food/food.gd
+++ project/food/food.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
-var foods = ["res://assets/Food/Dessert/cake_1.png","res://assets/Food/Dessert/cake_2.png",
- 			"res://assets/Food/Dessert/cracker.png","res://assets/Food/Dessert/easter_cake.png",
-			"res://assets/Food/Dessert/ice_cream_1.png","res://assets/Food/Dessert/ice_cream_2.png",
-			"res://assets/Food/Dessert/muffin_1.png","res://assets/Food/Dessert/muffin_2.png",
-			"res://assets/Food/Dessert/pancakes.png","res://assets/Food/Dessert/roll.png"]
+
+@onready var food_sp = $food
+
+var foods = [load("res://assets/Food/Dessert/cake_1.png"),load("res://assets/Food/Dessert/cake_2.png"),
+ 			load("res://assets/Food/Dessert/cracker.png"),load("res://assets/Food/Dessert/easter_cake.png"),
+			load("res://assets/Food/Dessert/ice_cream_1.png"),load("res://assets/Food/Dessert/ice_cream_2.png"),
+			load("res://assets/Food/Dessert/muffin_1.png"),load("res://assets/Food/Dessert/muffin_2.png"),
+			load("res://assets/Food/Dessert/pancakes.png"),load("res://assets/Food/Dessert/roll.png")]
+
+
