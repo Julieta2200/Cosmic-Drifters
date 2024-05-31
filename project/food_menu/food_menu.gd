d500b52c0d1386f1b3896a49bb32b3967a480187
Julieta2200 <julieta@megista.io>
2024-05-31 17:38:51 +0400

    main panel

--- project/food_menu/food_menu.gd
+++ project/food_menu/food_menu.gd
@@ -1,11 +1,30 @@
 extends Control
 
+func _on_hot_dishes_gui_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			$main_panel/hot_dishes.texture = load("res://assets/Menu_UI/Panel_Hot_Dishes/Hot_dishes1.png")
+			$main_panel/dessert.texture = load("res://assets/Menu_UI/Panel_Dessert/Dessert2.png")
+			$main_panel/drinks.texture = load("res://assets/Menu_UI/Panel_Drinks/Drinks2.png")
+			$main_panel.texture = load("res://assets/Menu_UI/Menu/Menu1.png")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_dessert_gui_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			$main_panel/hot_dishes.texture = load("res://assets/Menu_UI/Panel_Hot_Dishes/Hot_dishes2.png")
+			$main_panel/dessert.texture = load("res://assets/Menu_UI/Panel_Dessert/Dessert1.png")
+			$main_panel/drinks.texture = load("res://assets/Menu_UI/Panel_Drinks/Drinks2.png")
+			$main_panel.texture = load("res://assets/Menu_UI/Menu/Menu2.png")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_drinks_gui_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			$main_panel/hot_dishes.texture = load("res://assets/Menu_UI/Panel_Hot_Dishes/Hot_dishes2.png")
+			$main_panel/dessert.texture = load("res://assets/Menu_UI/Panel_Dessert/Dessert2.png")
+			$main_panel/drinks.texture = load("res://assets/Menu_UI/Panel_Drinks/Drinks1.png")
+			$main_panel.texture = load("res://assets/Menu_UI/Menu/Menu3.png")
+
+
