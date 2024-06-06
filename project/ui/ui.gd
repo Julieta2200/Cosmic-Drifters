a194b4ab7247e432ab014951d242c1770f4521e4
Julieta2200 <julieta@megista.io>
2024-06-06 20:55:51 +0400

    item

--- project/ui/ui.gd
+++ project/ui/ui.gd
@@ -1,11 +1,11 @@
 extends Control
 
 var add_minutes = 15
+var items = {}
 
 func _ready():
 	$time.text = String("09:00")
 
-	
 func get_time() -> String:
 	var time = $time.text.split(":")
 	var hour = int(time[0])
@@ -22,3 +22,21 @@ func get_time() -> String:
 
 func _on_timer_timeout():
 	$time.text = get_time()
+
+
+func add_item(table):
+	var item_index = 0
+	for item in $item.get_children():
+		item_index  =  item_index + 1
+		if item.get_child(0).text == null:
+			item.get_child(0).text = "N : " + str(table.number)
+			items.append({ item_index : table})
+			return 
+
+func get_table(index):
+	return items[index]
+
+func remove_item(item_index):
+	$item.get_child(item_index).get_child(0).text = ""
+	items.erase(item_index)
+	
