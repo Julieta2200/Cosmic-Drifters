d07f82774a923aeb82b452df957ad81568a092f5
grishamegista <grisha@megista.io>
2024-06-12 18:29:18 +0400

    loading screen

--- project/loading_screen/loading_screen.gd
+++ project/loading_screen/loading_screen.gd
@@ -1,18 +1,15 @@
-extends CanvasLayer
+extends Node2D
 
 signal loading_screen_has_full_coverage
 
-@onready var animationPlayer : AnimationPlayer = $AnimationPlayer
-@onready var progressBar : ProgressBar = $Panel/ProgressBar
+func _ready():
+	emit_signal("loading_screen_has_full_coverage")
 
 func _update_progress_bar(new_value: float) -> void:
-	progressBar.set_value_no_signal(new_value * 100)
+	pass
 	
 func _start_outro_animation() -> void:
-	animationPlayer.play("end_load")
+	self.queue_free()
 
 
-func _on_animation_player_animation_finished(anim_name):
-	match anim_name:
-		"end_load":
-			self.queue_free()
+			
