cfa4d5a6716428f988918cca23d1074b31a4024b
GorMegista <gor@megista.io>
2024-06-17 22:02:27 +0400

    dialog animation crated

--- project/dialog/dialog.gd
+++ project/dialog/dialog.gd
@@ -1,10 +1,25 @@
 class_name Dialog extends Control
 
+var showNameIndex = 0
+var fname = ""
+var showTextIndex = 0
+var ftext = ""
+
 func appear(text: String , name :String , sprite : Texture):
-	$background/text.text = text
-	$background/name.text = name
+	ftext = text
+	fname = name
+	showTextIndex = 0
+	showNameIndex = 0
 	$background/stroke/sprite.texture = sprite
 	visible = true
 	
 func disappear():
 	visible = false
+
+func _process(delta):
+	if showNameIndex <= fname.length():
+		$background/name.text = fname.substr(0, showNameIndex)
+		showNameIndex += 1
+	elif showTextIndex <= ftext.length():
+		$background/text.text = ftext.substr(0, showTextIndex)
+		showTextIndex += 1
