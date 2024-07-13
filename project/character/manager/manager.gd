8a7d8d8d4774002d159b0071114bf06fef648d21
Julieta2200 <julieta@megista.io>
2024-07-13 21:25:34 +0400

    walk to room

--- project/character/manager/manager.gd
+++ project/character/manager/manager.gd
@@ -1,18 +1,10 @@
 class_name Manager extends Waiter
 
-signal walk_to_room
-
 var serve_table
 var room_position
 
 func _physics_process(_delta):
 	move()
-	if serve_table != null:
-		if serve_table.group.current_status == serve_table.group.STATUS_SERVE_END:
-			busy = true
-			walk_to(room_position, walk_to_room)
-			serve_table = null
-			
 
 func _ready():
 	position_delta = speed / 60
@@ -25,7 +17,12 @@ func walk_to_give_check(table: Table):
 	busy = true
 	var serve_point = table.get_serve_point()
 	walk_to(serve_point, ready_give_check , table)
-	serve_table = table
 
-func _on_walk_to_room():
-	pass
+func walk_to_room():
+	busy = true
+	walk_to(room_position, any)
+
+func _on_ready_give_check(table: Table):
+	super._on_ready_give_check(table)
+	walk_to_room()
+	
