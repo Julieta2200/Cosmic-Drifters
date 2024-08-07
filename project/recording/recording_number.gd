086fb59b1573a7a032ee6dcf0911ccb889fbb34d
Julieta2200 <julieta@megista.io>
2024-08-07 17:05:17 +0400

    recording panel

---
+++ project/recording/recording_number.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var stop_button = ["res://assets/recording/Recording Panel/Stop_button/Stop1.png", 
+					"res://assets/recording/Recording Panel/Stop_button/Stop2.png"]
+
+var play_button = ["res://assets/recording/Recording Panel/Play_button/Play1.png",
+					"res://assets/recording/Recording Panel/Play_button/Play2.png"]
+
+@onready var button = $play_button
+
+
+func _on_play_button_pressed():
+	if button.texture_normal == load(play_button[0]):
+		$recording_animation.play("recording")
+		button_texture(stop_button)
+	else:
+		$recording_animation.stop()
+		button_texture(play_button)
+		
+	
+func button_texture(button_texture):
+	button.texture_normal = load(button_texture[0])
+	button.texture_pressed = load(button_texture[1])
+
