36364722830ec01226d643c9a4c03415c2665c8e
Julieta2200 <julieta@megista.io>
2024-08-07 17:45:27 +0400

    button state

--- project/recording/recording_number.gd
+++ project/recording/recording_number.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var butten_state : bool
+
 var stop_button = ["res://assets/recording/Recording Panel/Stop_button/Stop1.png", 
 					"res://assets/recording/Recording Panel/Stop_button/Stop2.png"]
 
@@ -7,15 +9,19 @@ var play_button = ["res://assets/recording/Recording Panel/Play_button/Play1.png
 					"res://assets/recording/Recording Panel/Play_button/Play2.png"]
 
 @onready var button = $play_button
+@onready var recording_animation = $recording_animation
 
 
 func _on_play_button_pressed():
+	$"../..".stoping()
 	if button.texture_normal == load(play_button[0]):
-		$recording_animation.play("recording")
+		recording_animation.play("recording")
 		change_texture(stop_button)
+		butten_state = true
 	else:
-		$recording_animation.stop()
+		recording_animation.stop()
 		change_texture(play_button)
+		butten_state = false
 		
 	
 func change_texture(button_texture):
